c515a6920ec1e96e5b1eef889488b61c23bfb439
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-23 20:01:37 -0300

    first commit

---
+++ src/test/java/com/fiap/dataset/DatasetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.dataset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
