85e134548f222aee93ac33b0428d61da7b309c02
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-23 21:31:52 -0300

    script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into objetivo(id) values ('3.1');
+insert into indicador(id, descricao) values ('3.1.1', 'Razão de mortalidade materna');
+insert into indicador(id, descricao) values ('3.1.2', 'Proporção de nascimentos assistidos por profissional de saúde qualificado');
